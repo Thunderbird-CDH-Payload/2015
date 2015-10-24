4a4f9fe1c29a2737dfc8d6d4daa260f7fd9a6307
aschereT <aschereT@users.noreply.github.com>
2015-10-24 14:14:40 -0700

    Important update

--- Android/Test.c
+++ Android/Test.c
@@ -1,17 +1,28 @@
 #include <iostream>
 
-void main()
+int main()
 {
-  for(int n = 99; n => 0; n--)
+  for(int n = 99; n >= 0; n--)
   {
-    if(n!=0)
+    if(n>2)
     {
       std::cout << n << " cubesats on low earth orbit, " << n << " cubesats" << std::endl;
-      std::cout << "Shoot one down with a laser, it de-orbits, " << n-- << " cubesats";
+      std::cout << "Shoot one down with a laser, it de-orbits, " << n-1 << " cubesats" << std::endl;
+    }
+    else if(n==2)
+    {
+        std::cout << n << " cubesats on low earth orbit, " << n << " cubesats" << std::endl;
+        std::cout << "Shoot one down with a laser, it de-orbits, " << n-1 << " cubesat" << std::endl;
+    }
+    else if(n==1)
+    {
+        std::cout << "One cubesat on low earth orbit, one cubesat" << std::endl;
+        std::cout << "Shoot one down with a laser, it de-orbits, no cubesats" << std::endl;
     }
     else
     {
-      std::cout << "No more cubesates on low earth orbit, no more cubesats!" << std::endl;
+      std::cout << "No more cubesats on low earth orbit, no more cubesats!" << std::endl;
     }
   }
+  return 0;
 }
