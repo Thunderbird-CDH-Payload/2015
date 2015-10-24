b8678aba1e6d0483cf5ff08deba8b4cfc1d23144
aschereT <aschereT@users.noreply.github.com>
2015-10-24 14:06:35 -0700

    Update Test.c

--- Android/Test.c
+++ Android/Test.c
@@ -2,5 +2,16 @@
 
 void main()
 {
-  std::cout<<"Test"<<std::endl;
+  for(int n = 99; n => 0; n--)
+  {
+    if(n!=0)
+    {
+      std::cout << n << " cubesats on low earth orbit, " << n << " cubesats" << std::endl;
+      std::cout << "Shoot one down with a laser, it de-orbits, " << n-- << " cubesats";
+    }
+    else
+    {
+      std::cout << "No more cubesates on low earth orbit, no more cubesats!" << std::endl;
+    }
+  }
 }
