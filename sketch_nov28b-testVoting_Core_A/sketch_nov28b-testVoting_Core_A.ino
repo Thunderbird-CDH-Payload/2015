b183200de5ed74898a78bc728a89d0937f16e695
Bo <bohu8@hotmail.com>
2015-11-28 17:02:11 -0800

    fixed indenting in arduino code

--- sketch_nov28b-testVoting_Core_A/sketch_nov28b-testVoting_Core_A.ino
+++ sketch_nov28b-testVoting_Core_A/sketch_nov28b-testVoting_Core_A.ino
@@ -1,5 +1,5 @@
 //Test voting array
-//A 
+//A
 
 //read from core from serial port of other two core
 //data array from different boards
@@ -8,61 +8,61 @@ char Adata[32];
 
 
 void setup() {
- 
-   //serial comms startup
-  Serial.begin(9600);
-  Serial1.begin(9600); //A comms Trill
-  Serial2.begin(9600); //A comms B
-
+    
+    //serial comms startup
+    Serial.begin(9600);
+    Serial1.begin(9600); //A comms Trill
+    Serial2.begin(9600); //A comms B
+    
 }
 
 void loop() {
-  Serial.print("going to call voting array");
-  votingArray();
+    Serial.print("going to call voting array");
+    votingArray();
 }
 
 void votingArray(){
-delay(5000);
-//Read data from Trill
-Serial.print("going to call getSignalDatat");
- getSignalData();
- 
-delay(5000);
-Serial.print("going to write to B");
-
-writeB();
-
-delay(200);
-
-Serial.print("going to read from B");
-readB(); //reading B from serial
+    delay(5000);
+    //Read data from Trill
+    Serial.print("going to call getSignalDatat");
+    getSignalData();
+    
+    delay(5000);
+    Serial.print("going to write to B");
+    
+    writeB();
+    
+    delay(200);
+    
+    Serial.print("going to read from B");
+    readB(); //reading B from serial
 }
 
 //Gets signal
 void getSignalData(){
-  Serial.print("Got data from Main:");
-  int a=0;
-while(Serial1.available()>0){  
-  Adata[a]==Serial1.read();
-  Serial.print(Adata[a]);
-  a++;}
-  Adata[a]='\0';
-  }
+    Serial.print("Got data from Main:");
+    int a=0;
+    while(Serial1.available()>0){
+        Adata[a]==Serial1.read();
+        Serial.print(Adata[a]);
+        a++;}
+    Adata[a]='\0';
+}
 
 //A writes to B
 void writeB(){
-      Serial.print("writing to B");   
-      Serial2.write(Adata);
-    }
+    Serial.print("writing to B");
+    Serial2.write(Adata);
+}
 
 //Get B's data
 void readB(){
-  int b=0;
-  Serial.print("Getting B's data:");
-  while (Serial2.available() > 0){ 
-
-    Bdata[b] = Serial2.read();
-    b++;}    
-    Bdata[b] = '\0';    
-   }
+    int b=0;
+    Serial.print("Getting B's data:");
+    while (Serial2.available() > 0){
+        
+        Bdata[b] = Serial2.read();
+        b++;}
+    Bdata[b] = '\0';
+}
 
--- sketch_nov28ctestingVoting_-_Core_B/sketch_nov28ctestingVoting_-_Core_B.ino
+++ sketch_nov28ctestingVoting_-_Core_B/sketch_nov28ctestingVoting_-_Core_B.ino
@@ -1,5 +1,5 @@
 //Test voting array
-//A 
+//A
 
 
 //read from core from serial port of other two core
@@ -9,74 +9,74 @@ char Adata[32];
 
 
 void setup() {
-
-  //serial comms startup
-  Serial.begin(9600);
-  Serial1.begin(9600); //B comms Trill
-  Serial2.begin(9600); //B commes A
-
+    
+    //serial comms startup
+    Serial.begin(9600);
+    Serial1.begin(9600); //B comms Trill
+    Serial2.begin(9600); //B commes A
+    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("going to call voting array");
-  votingArray();
-
+    // put your main code here, to run repeatedly:
+    Serial.print("going to call voting array");
+    votingArray();
+    
 }
 
 
 //if theres data is going to be sent  call this
 //TODO check if there is data needed to be compared
 void votingArray(){
-delay(5000);
-//Read data from Trill
-Serial.print("going to call getSignalDatat");
- getSignalData();
- 
-delay(5000);
+    delay(5000);
+    //Read data from Trill
+    Serial.print("going to call getSignalDatat");
+    getSignalData();
+    
+    delay(5000);
+    
+    Serial.print("going to write to B");
+    
+    writeA();
+    
+    delay(200);
+    
+    Serial.print("going to read from B");
+    readA(); //reading A from serial
+    
+    
+    delay(200);
+}
 
-Serial.print("going to write to B");
-
-writeA();
-
-delay(200);
-
-Serial.print("going to read from B");
-readA(); //reading A from serial
-
-
-delay(200);
-  }
-
-  //Gets signal
+//Gets signal
 void getSignalData(){
-  int a=0;
-  Serial.print("Got data from Main:"); 
-while(Serial1.available()>0){ 
-  Bdata[a]==Serial1.read();
-  Serial.print(Bdata[a]);
-  a++;}
-  Bdata[a]='\0';
-  }
+    int a=0;
+    Serial.print("Got data from Main:");
+    while(Serial1.available()>0){
+        Bdata[a]==Serial1.read();
+        Serial.print(Bdata[a]);
+        a++;}
+    Bdata[a]='\0';
+}
 
 
 //B writes to A
 void writeA(){
-    Serial.print("writing to A");   
-      Serial2.write(Bdata);
-
+    Serial.print("writing to A");
+    Serial2.write(Bdata);
+    
 }
 
 
 //Get A's data
 void readA(){
-  int a=0;
-  Serial.println("Getting A's data:");
-  while (Serial2.available() > 0){ 
-
-    Adata[a] = Serial2.read();
-    a++;}    
+    int a=0;
+    Serial.println("Getting A's data:");
+    while (Serial2.available() > 0){ 
+        
+        Adata[a] = Serial2.read();
+        a++;}    
     Adata[a] = '\0';    
-  }
+}
 
 
