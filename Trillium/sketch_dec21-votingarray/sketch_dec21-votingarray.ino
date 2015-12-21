795288e5d452c77a475f282b56fd6084fe46ebca
Bo <bohu8@hotmail.com>
2015-12-21 15:23:12 -0800

    voting array is working

---
+++ Trillium/sketch_dec21-votingarray/sketch_dec21-votingarray.ino
@@ -0,0 +1,132 @@
+//testing voting array 
+//CORE A
+
+#define TRUE 1
+#define FALSE 0
+
+#define WAIT_TIME 50
+
+#define BUFFSIZE 64
+
+#define SERIAL_RATE 115200
+
+#define CHAR1 'A'
+#define CHAR2 'A'
+#define END_CHAR 'Z'
+
+char Adata[BUFFSIZE];
+char Bdata[BUFFSIZE];
+char Cdata[BUFFSIZE];
+
+int ac = 0;
+int ab = 0;
+
+void setup() {
+  Serial.begin(SERIAL_RATE);  
+  Serial1.begin(SERIAL_RATE);
+  Serial2.begin(SERIAL_RATE);
+  Serial3.begin(SERIAL_RATE);  
+}
+
+void loop() {
+  votingArray();
+}
+
+void votingArray(){
+  clearArray(Adata, BUFFSIZE);
+  clearArray(Bdata, BUFFSIZE);
+  clearArray(Cdata, BUFFSIZE);
+  if (getSignalData()){
+    writeOthers();
+    delay(WAIT_TIME);
+    readB();
+    readC();
+    ab = different(Adata, Bdata, BUFFSIZE);
+    Serial.print("AB Compare: ");
+    Serial.println(ab);
+    ac = different(Adata, Cdata, BUFFSIZE);
+    Serial.print("AC Compare: ");
+    Serial.println(ac);   
+    
+  }  
+}
+
+int getSignalData(){
+  delay(WAIT_TIME);
+  clearArray(Adata, BUFFSIZE);
+  int m=0;
+  while(Serial1.available() > 0){
+    char temp = Serial1.read();
+    Adata[m] = temp;
+    m++;
+    if (temp == END_CHAR)
+      break;
+  }
+  Serial1.flush();
+  Adata[m]='\0';
+  // Adata[4] = 'B';
+  if (Adata[0] == CHAR1 && Adata[1] == CHAR2){
+    Serial.print("Got Data from main: ");
+    Serial.println(Adata); //printing to screen
+    return TRUE;
+  }
+  return FALSE;
+}
+
+void writeOthers(){
+  Serial1.print(Adata);
+}
+
+int readB(){
+  clearArray(Bdata, BUFFSIZE);
+  int m=0;
+  while(Serial2.available() > 0){
+    char temp = Serial2.read();
+    Bdata[m] = temp;
+    m++;
+    if (temp == END_CHAR)
+      break;
+  }
+  Serial2.flush();
+  Bdata[m]='\0';
+  if (Bdata[0] == CHAR1 && Bdata[1] == CHAR2){
+    Serial.print("Got Data from B: ");
+    Serial.println(Bdata); //printing to screen
+    return TRUE;
+  }
+  return FALSE;
+}
+
+int readC(){
+  clearArray(Cdata, BUFFSIZE);
+  int m=0;
+  while(Serial3.available() > 0){
+    char temp = Serial3.read();
+    Cdata[m] = temp;
+    m++;
+    if (temp == END_CHAR)
+      break;
+  }
+  Serial3.flush();
+  Cdata[m]='\0';
+  if (Cdata[0] == CHAR1 && Cdata[1] == CHAR2){
+    Serial.print("Got Data from C: ");
+    Serial.println(Cdata); //printing to screen
+    return TRUE;
+  }
+  return FALSE;
+}
+
+int different(char* a1, char* a2, int n){
+  for(int i = 0; i < n; i++){
+    if (a1[i] != a2[i])
+      return TRUE;
+  }
+  return FALSE;
+}
+
+void clearArray(char* a, int n){
+  for(int i = 0; i < n; i++){
+    a[i] = '\0';
+  } 
+}
