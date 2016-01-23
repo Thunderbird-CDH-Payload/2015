70905500e12d5de489496fb1a8d70c043a7f48a1
Bo <bohu8@hotmail.com>
2016-01-23 15:20:15 -0800

    removed else for error

--- Trillium/Arduino-demo-version/Board-demo/Board-demo.ino
+++ Trillium/Arduino-demo-version/Board-demo/Board-demo.ino
@@ -31,7 +31,7 @@ Software for controlling radiation redundency in the Trillium architechture. Cod
 #include "avr/sleep.h"
 
 //*** CHANGE THIS NUMBER BEFORE UPLOADING SKETCH ***
-#define ARDUINO_ID 3
+#define ARDUINO_ID 2
 
 //***VOTING FN USE
 
@@ -186,9 +186,7 @@ void votingArray(){
     
     if (errbit>0) {
       simulateError(errNum);
-    }
-            
-    else {    
+    }    
       // write the received data from host to the other 2 Arduinos
       writeOthers();
       delay(WAIT_TIME);
@@ -221,7 +219,7 @@ void votingArray(){
       // drive reset pins back low
       digitalWrite(Breset, LOW);
       digitalWrite(Creset, LOW);
-    }
+      
     sendDataToMain(); //SENDING TO MAIN  
 
   
